2511d68abf19ae3ca9c84a90c34a618bdd9c82d0
Phi Do <76242187+PhiHDo@users.noreply.github.com>
2022-03-13 23:57:10 -0700

    added functionality (js elements) to the search bar

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 const pokedex = document.getElementById('pokedex');
+const searchBar = document.getElementById('searchBar');
+let pokemon = []; //array of pokemon from the fetch pokemon function
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 150; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name, 
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
+//Search bar
+//allows user to search for pokemon
+searchBar.addEventListener('keyup', (e) => {
+    //search is not case sensitive
+    //convert name to lowercase and then compare
+    const searchString = e.target.value.toLowerCase();
+    const filteredPokemon = pokemon.filter((pokemon) => {//returns array of filter pokemon
+        return (
+            pokemon.name.toLowerCase().includes(searchString)
+        );
+    });
+    displayPokemon(filteredPokemon);
+});
+
+//fetches data on pokemon from the pokemon api
+const fetchPokemon = async () => {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150');
+        data = await res.json();
+        pokemon = data.results.map( (results , index)  => ({
+            name: results.name,
+            id: index + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
         }));
         displayPokemon(pokemon);
-    });
+    } catch (err) {
+        console.error(err);
+    }
 };
-
+    
+//displays Pokemon's image, name, and type
 const displayPokemon = (pokemon) => {
     console.log(pokemon);
     const pokemonHTMLString = pokemon
-        .map(
-            (pokeman) => `
+        .map((pokeman) => `
         <li class="card">
             <img class="card-image" src="${pokeman.image}"/>
             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
