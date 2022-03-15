ab3cbdb818b0a94047aba6684bbb19338a70da0c
Phi Do <76242187+PhiHDo@users.noreply.github.com>
2022-03-14 18:24:53 -0700

    added comments to the js file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const pokedex = document.getElementById('pokedex');
 const searchBar = document.getElementById('searchBar');
-let pokemon = [];
+let pokemon = []; //array of pokemon from the fetch pokemon function
 
+//Search bar
+//allows user to search for pokemon
 searchBar.addEventListener('keyup', (e) => {
-
+    //search is not case sensitive
+    //convert name to lowercase and then compare
     const searchString = e.target.value.toLowerCase();
-    const filteredPokemon = pokemon.filter((pokemon) => {
+    const filteredPokemon = pokemon.filter((pokemon) => {//returns array of filter pokemon
         return (
             pokemon.name.toLowerCase().includes(searchString) 
             || pokemon.id <= searchString
@@ -14,6 +17,7 @@ searchBar.addEventListener('keyup', (e) => {
     displayPokemon(filteredPokemon);
 });
 
+//fetches data on pokemon from the pokemon api
 const loadPokemon = async () => {
     try {
         const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=550');
@@ -31,6 +35,7 @@ const loadPokemon = async () => {
     }
 };
 
+//displays Pokemon's image, name, and type
 const displayPokemon = (pokemon) => {
     const pokemonHTMLstring = pokemon
     .map (
@@ -52,6 +57,7 @@ const selectPokemon = async(id) => {
     displayWindow(pokeman);
 };
 
+//pop up window
 const displayWindow = (pokeman) => {
     const type = pokeman.types.map((type) => 
     type.type.name).join(', ');
@@ -76,6 +82,7 @@ const displayWindow = (pokeman) => {
     console.log(pokeman);
 };
 
+//close the pop up window
 const closePopup = () => {
     const popup = document.querySelector('.popup');
     popup.parentElement.removeChild(popup);
